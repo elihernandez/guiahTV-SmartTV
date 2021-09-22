de2bf69fb58180f3a3def70ed40e2a640a4fef29
elihernandez <nhernandez@lap55.com>
2021-09-22 09:49:47 -0500

    edit - se elimina solid js de la rama

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@
 let topMenu = document.getElementById('top-menu')
 topMenu.parentNode.removeChild(topMenu)
 
-import { render } from 'solid-js/web'
-import { createSignal, onCleanup, createEffect, createMemo, createResource } from 'solid-js'
-// import 'babel-polyfill'
-// import ReactDOM from 'react-dom'
-// import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'
-// import App from './App'
+import 'babel-polyfill'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 // require('./js/dist/bcrypt')
 // require('./js/dist/hls')
@@ -16,21 +13,7 @@ import { createSignal, onCleanup, createEffect, createMemo, createResource } fro
 // require('./js/dist/slick')
 
 
-const App = () => {
-	const [getData, setData] = createSignal(0)
-	console.log(getData())
-	setData(2)
-	console.log(getData())
-	
-	return <h1>Hola mundo</h1>
-}
-
-render(
-	App,
+ReactDOM.render(
+	<App />,
 	document.getElementById('app')
 )
-
-// ReactDOM.render(
-// 	<App />,
-// 	document.getElementById('app')
-// )
