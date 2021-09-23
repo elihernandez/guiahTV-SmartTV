4931d072fc7f9082b78ca449e4934d56f6366fc7
elihernandez <nhernandez@lap55.com>
2021-09-23 12:53:50 -0500

    Se eliminan archivos de compilación

--- webOS/service/com.guiahtv.smarttv.updateversioncodeService/main.js
+++
@@ -1,8 +0,0 @@
-var Service = require('webos-service');
-var service = new Service("com.guiahtv.smarttv.updateversioncode");
-var fs = require('file-system');
-const axios = require('axios');
-
-service.register("updateVersion", function(message) {
-	
-});
