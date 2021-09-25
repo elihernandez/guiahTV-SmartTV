a990a6337dce037f2d66b48b99a838dc803a82b6
elihernandez <nhernandez@lap55.com>
2021-09-24 23:01:19 -0500

    edit - se elimina código innecesario

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,3 @@
-console.log(window.location.href)
-
 import Navigo from 'navigo'
 export const url = window.location.href
 export const router = new Navigo('/', {
@@ -7,12 +5,3 @@ export const router = new Navigo('/', {
 })
 // export const router = new Navigo('media/developer/apps/usr/palm/applications/com.guiahtv.smarttv/')
 // export const router = new Navigo(`${window.location.href}/`)
-
-// import Router from 'vanilla-router'
-
-// export const router = new Router({
-// 	mode: 'history',
-// 	page404: function (path) {
-// 		console.log('"/' + path + '" Page not found')
-// 	}
-// })
