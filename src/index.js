8800103cb004d1808726ce9297eeacb7c7cbde46
elihernandez <nhernandez@lap55.com>
2021-09-24 23:00:20 -0500

    add - se agrega componente Main con loader logo y loader video

--- src/index.js
+++ src/index.js
@@ -6,21 +6,45 @@ $('#top-menu').parentNode.removeChild($('#top-menu'))
 $('#app').innerHTML = ''
 
 import 'babel-polyfill'
-import { render } from 'solid-js/web'
+import { getState } from './state'
 import App from './App'
-
+import { LogoLoader, VideoLoader } from './components/Loader'
+import { onMount } from 'solid-js'
+import { fadeInElement } from './utils/transition'
+import { render } from 'solid-js/web'
 // require('./js/dist/bcrypt')
 // require('./js/dist/hls')
 // require('./js/dist/js-cookie')
-// require('./js/dist/jquery')
-// require('./js/dist/slick')
+
+function Main(){
+	const { isShowVideo } = getState().appState
+	
+	onMount(() => {
+		let loader
+		if(isShowVideo){
+			loader = $('.video-loader')
+			fadeInElement(loader, '0', '1', '150ms')
+			$('#video-logo').play()
+		}else{
+			loader = $('.logo-loader')
+			fadeInElement(loader, '0', '1', '150ms')
+		}
+	})
+
+	return (
+		<div className="main-loader-app">
+			<LogoLoader />
+			<VideoLoader />
+		</div>
+	)
+}
 
 render(
-	() => <App />,
+	() => (
+		<>
+			<Main />
+			<App />
+		</>
+	),
 	document.getElementById('app')
 )
-
-// render(
-// 	<App />,
-// 	document.getElementById('app')
-// )
