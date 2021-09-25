d9ab5d666ab9f80f91f601851b50ec17a6619355
elihernandez <nhernandez@lap55.com>
2021-09-24 22:59:19 -0500

    add - se agrega componente video loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,9 +1,16 @@
 import './styles.css'
 import logo from '../../assets/images/logos/guiahtv/guiahtvlogo.png'
+import video from '../../assets/videos/video-logo.mp4'
 
 export const LogoLoader = () => {
-	return <div className="loader logo-loader" style={{ 'opacity': '1', 'display': '' }}>
+	return <div className="loader logo-loader" style={{ 'opacity': '0', 'display': 'none' }}>
 		<img src={logo} alt="Logo guiah tv" />
+	</div>
+}
+
+export const VideoLoader = () => {
+	return <div className="loader video-loader" style={{ 'opacity': '0', 'display': 'none' }}>
+		<video muted id="video-logo" src={video} />
 	</div>
 }
 
