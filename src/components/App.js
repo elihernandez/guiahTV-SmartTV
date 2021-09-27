575cf521b9c0285a6667f7f7830d643fd33af78d
elihernandez <nhernandez@lap55.com>
2021-09-27 14:45:02 -0500

    edit - se elimina código innecesario

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import { getState, subscribe } from '../state'
+import { router } from '../router'
+import HomePage from '../pages/Home'
+import { onMount } from 'solid-js'
+// import LiveTvPage from './pages/LiveTv'
+import Menu from './Menu'
+import VodPage from '../pages/Vod'
+// import RadioPage from './pages/Radio'
+// import MusicPage from './pages/Music'
+// import ZonaKids from './pages/ZonaKids'
+import { MainLoader } from './Loader'
+import { fadeInElement, fadeOutElement } from '../utils/transition'
+import { $ } from '../utils/dom'
+
+export const App = () => {
+	const { isShowVideo } = getState().appState
+	
+	function loadedApp(){
+		router.navigate('/home')
+		fadeInElement($('.main-section'), '0', '1', '150', '150')
+
+		isShowVideo
+			? fadeOutElement($('.video-loader'), '1', '0', '150', '6500')
+			: fadeOutElement($('.logo-loader'), '1', '0', '150', '1500')
+	}
+
+	onMount(() => {
+		subscribe(loadedApp, state => state.appState.loadedApp)
+	})
+
+	return (
+		<div className="main-section" style={{ 'opacity': '0', 'display': 'none' }}>
+			<Menu />
+			<HomePage />
+			<VodPage />
+			{/* 
+			<LiveTvPage />
+			<RadioPage />
+			<MusicPage />
+			<ZonaKids />
+			 */}
+			<MainLoader />
+		</div>
+	)
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,49 +1,23 @@
 import './assets/iconcolor80x80.png'
 import './assets/iconcolor130x130.png'
-import { $ } from './utils/dom'
-
-$('#top-menu').parentNode.removeChild($('#top-menu'))
-$('#app').innerHTML = ''
+const topMenu = document.getElementById('top-menu')
+topMenu.parentNode.removeChild(topMenu)
+document.getElementById('app').innerHTML = ''
 
 import 'babel-polyfill'
-import { getState } from './state'
-import App from './App'
-import { LogoLoader, VideoLoader } from './components/Loader'
-import { onMount } from 'solid-js'
+import MainLoaders from './components/MainLoaders'
+import App from './components/App'
 import { render } from 'solid-js/web'
-import { fadeInElement } from './utils/transition'
+import './styles.css'
 // require('./js/dist/bcrypt')
 // require('./js/dist/hls')
 // require('./js/dist/js-cookie')
-
-function Main(){
-	const { isShowVideo } = getState().appState
-	
-	onMount(() => {
-
-		function showVideo(){
-			fadeInElement($('.video-loader'), '0', '1', '150')
-			$('#video-logo').play()
-		}
-
-		isShowVideo
-			? showVideo()
-			: fadeInElement($('.logo-loader'), '0', '1', '150')
-	})
-
-	return (
-		<>
-			<LogoLoader />
-			<VideoLoader />
-		</>
-	)
-}
 
 render(
 	() => (
 		<>
 			<App />
-			<Main />
+			<MainLoaders />
 		</>
 	),
 	document.getElementById('app')
