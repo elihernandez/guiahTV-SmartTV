6d3335fa0ab215c2916d892fd0ad299026d45d5c
elihernandez <nhernandez@lap55.com>
2021-09-27 12:23:27 -0500

    update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -11,28 +11,11 @@ module.exports = function(api) {
 					chrome: '38',
 				},
 			}
-		],
-		// [
-		// 	'@babel/preset-react',
-		// 	{
-		// 		runtime: 'automatic'
-		// 	}
-		// ]
-	]
-
-	const plugins = [
-		// 'transform-remove-strict-mode',
-		// '@babel/plugin-transform-async-to-generator',
-		// '@babel/plugin-proposal-optional-chaining'
-		// '@babel/plugin-transform-arrow-functions'
-		// '@babel/plugin-transform-react-constant-elements',
-		// '@babel/plugin-transform-react-inline-elements',
-		// '@babel/plugin-proposal-export-default-from'
+		]
 	]
 
 	return {
 		'compact': false,
-		presets,
-		plugins
+		presets
 	}
 }
