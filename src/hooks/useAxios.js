811442e75dcfdd968308142bbb0bf0b06cff8934
elihernandez <nhernandez@lap55.com>
2021-09-28 17:43:15 -0500

    Add function to validate error and return component with message error

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,7 +1,8 @@
 import axios from '../utils/axios'
 // import { useState, useCallback } from 'react'
 // import { useSelector } from 'react-redux'
-// import * as ErrorComponent from '../components/ErrorMessage'
+import { createSignal } from 'solid-js'
+import * as Error from '../components/Error'
 // import { createSignal, createMemo } from 'solid-js'
 import { getURL } from '../api/endpoints'
 import { validateSuscription, validateError } from '../utils/auth'
@@ -9,6 +10,7 @@ import { validateSuscription, validateError } from '../utils/auth'
 
 export default function useAxios(){
 	// const userToken = useSelector(state => state.user.userToken)
+	[getCount, setCount] = createSignal(0)
 	const userToken = ''
 	// const [loading, setLoading] = createSignal(true)
 	// const [count, setCount] = createSignal(0)
@@ -23,9 +25,18 @@ export default function useAxios(){
 		return await fetchData('get', section, params, body)
 	}
 
+	const incrementCount = () => {
+		const currentCount = getCount()
+
+		currentCount < 3
+			? setCount(currentCount + 1)
+			: setCount(0)
+
+		// console.log(getCount())
+	}
+
 	const fetchData = async (type, section, params, body) => {
 		try{
-			// setLoading(true)
 			const url = getURL(section, userToken, params)
 
 			const listFetchs = {
@@ -33,32 +44,33 @@ export default function useAxios(){
 			}
 		
 			const response = listFetchs[type]
-			// validateSuscription(response)
+			validateSuscription(response)
 			// setLoading(false)
 			return response
 		}catch(e){
-			const error = validateError(e)
-			console.log(error)
-			// setLoading(false)
+			const { error, code } = validateError(e)
+			// console.log(error)
+			// console.log(code)
 
-			// switch(code){
-			// case 0: // error del api
-			// 	throw(<ErrorComponent.ErrorMessage handleRequest={retryRequest} count={count} />)
-			// case 1: // error de sesión
-			// 	throw(<ErrorComponent.ErrorSession message={error} />)
-			// case 2: // error del cliente
-			// case 3: // error del servidor
-			// case 4: // network error
-			// case 5: // request aborted
-			// case 6: // timeout
-			// 	throw(<ErrorComponent.ErrorTimeout handleRequest={retryRequest} count={count} />)
-			// default: // error desconocido
-			// 	throw(<ErrorComponent.ErrorSession message={error} />)
-			// }
+			switch(code){
+			case 0: // error del api
+				// throw(<ErrorComponent.ErrorMessage handleRequest={retryRequest} count={count} />)
+			case 1: // error de sesión
+				throw(<Error.ErrorSession handleRequest={incrementCount} message={error} />)
+			case 2: // error del cliente
+			case 3: // error del servidor
+			case 4: // network error
+			case 5: // request aborted
+			case 6: // timeout
+				// throw(<ErrorComponent.ErrorTimeout handleRequest={retryRequest} count={count} />)
+			default: // error desconocido
+				// throw(<ErrorComponent.ErrorSession message={error} />)
+			}
 		}
 	}
 
 	return {
-		get
+		get,
+		getCount
 	}
 }
