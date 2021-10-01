91da7556f663784ff0c96c27e766ae6c271f1856
elihernandez <nhernandez@lap55.com>
2021-10-01 16:42:16 -0500

    add - se crea componente y se agrega fetch para data

--- src/pages/LiveTv/index.js
+++ src/pages/LiveTv/index.js
@@ -1,32 +1,57 @@
+import { createEffect, createSignal, Show } from 'solid-js'
 import useRouter from '../../hooks/useRouter'
 import useAxios from '../../hooks/useAxios'
 import { fadeInElement, fadeOutElement } from '../../utils/transition'
 import { $ } from '../../utils/dom'
 
+import Error from '../../components/Error'
+
 export default function LiveTv(){
-	const axios = useAxios(),
-		route = 'livetv',
+  const [getData, setData] = createSignal(null),
+		[getError, setError] = createSignal(null),
+		axios = useAxios(),
+		route = 'live-tv',
 		onBefore = () => {
-			const el = $('#livetv')
-			const loader  = $('.main-loader')
-			fadeInElement(loader, '0', '1', '150ms')
-			axios.get('livetv')
-				.then(response => {
-					console.log(response)
-					fadeInElement(el, '0', '1', '150ms')
-					fadeOutElement(loader, '1', '0', '150ms', '1000ms')
-				})
+			fetchData()
 		},
 		onLeave = () => {
-			const el = $('#livetv')
-			fadeOutElement(el, '1', '0', '150ms')
+			const page = $(`#${route}`)
+			fadeOutElement(page, '1', '0', '150ms')
+			setData(null)
+			axios.reset()
+		},
+		fetchData = async () => {	
+			const page = $(`#${route}`), loader  = $('.main-loader')
+			fadeInElement(loader, '0', '1', '150ms')
+			setError(null)
+
+			try{
+				const response = await axios.get('live-tv')
+				setData(response)
+			}catch(e){
+				setError(e)
+			}finally{
+				fadeInElement(page, '0', '1', '150')
+				fadeOutElement(loader, '1', '0', '150', '1000')
+			}
 		}
+
+	createEffect(() => {
+		if(axios.count() !== 0 && axios.count() <= 3){
+			fetchData()
+		}
+	})
 
 	useRouter({ route, onBefore, onLeave })
 
 	return (
-		<div className="section-wrapper" id="livetv" style={{ 'opacity': '0', 'display': 'none'}}>
-			<div className="welcome-message">Live TV</div>
+		<div className="section-wrapper" id={route} style={{ 'opacity': '0', 'display': 'none'}}>
+			{/* <Show when={getData()}>
+				<Catalogue data={getData()} />
+			</Show> */}
+			<Show when={getError()}>
+				<Error {...axios} />
+			</Show>
 		</div>
 	)
 }
